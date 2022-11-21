16fe7938c2cc482d605de88528c34afc8de94348
Rehaman-khan github <rehamankhan1603@gmail.comgit>
2022-11-21 23:00:17 +0530

    The given Number is Positive or Negative

---
+++ src/number_positive_or_negative.java
@@ -0,0 +1,17 @@
+
+public class number_positive_or_negative {
+	public static void main(String[] args) {
+		int num=0
+				;
+		if(num>0){
+			System.out.println("the number is positive");
+		}
+		else if(num<0){
+			System.out.println("the number is negative");
+	}
+		else {
+			System.out.println("the number i zero");
+		}
+
+}
+}
